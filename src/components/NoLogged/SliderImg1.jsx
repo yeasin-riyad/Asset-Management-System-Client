af0ef8d2f93db17cffbe521a7d04cab9a8800fd0
YeasinRiyad <mazumderyeasin98@gmail.com>
2024-11-15 21:08:17 +0600

    Some functionality update

--- src/components/NoLogged/SliderImg1.jsx
+++ src/components/NoLogged/SliderImg1.jsx
@@ -1,32 +1,34 @@
 import { Link } from "react-router-dom";
 
 const SliderImg1 = () => {
-    return (
-      <div className="relative">
-        <img
-          src="https://i.ibb.co/p0T4kZ2/HR-Director.jpg"
-          alt="Join as an HR"
-          className="w-full h-[450px] absolute inset-0 bg-black opacity-50"
-        />
-        <div className="absolute inset-0 flex flex-col items-center justify-center gap-y-4 top-64">
-          <h2 className="text-5xl text-gray-50">
-            Welcome to <span className="text-blue-700">TrackMyAssets</span>
-          </h2>
-          <p className="text-2xl text-gray-100">
-            {" "}
-            We've set up a sophisticated asset management system under experienced
-            and innovative professionals.
-          </p>
-          <Link to='/join-hr'
-            type="button"
-            className=" text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-lg px-7 py-3.5 me-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800"
-          >
-            Join as HR Manager
-          </Link>
-        </div>
+  return (
+    <div className="relative h-[450px]">
+      {/* Background image */}
+      <div
+        className="absolute inset-0 bg-cover bg-center opacity-50"
+        style={{
+          backgroundImage: "url('https://i.ibb.co.com/Gcj1tpy/pexels-divinetechygirl-1181408.jpg')",
+        }}
+      ></div>
+
+      {/* Content Section */}
+      <div className="absolute inset-0 flex flex-col items-center justify-center gap-y-4">
+        <h2 className="text-5xl text-gray-50">
+          Welcome to <span className="text-blue-700">TrackMyAssets</span>
+        </h2>
+        <p className="text-2xl text-gray-100">
+          We've set up a sophisticated asset management system under experienced
+          and innovative professionals.
+        </p>
+        <Link
+          to="/join-hr"
+          className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-lg px-7 py-3.5 me-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800"
+        >
+          Join as HR Manager
+        </Link>
       </div>
-    );
-  };
-  
-  export default SliderImg1;
-  
+    </div>
+  );
+};
+
+export default SliderImg1;
--- src/components/NoLogged/SliderImg2.jsx
+++ src/components/NoLogged/SliderImg2.jsx
@@ -1,32 +1,33 @@
 import { Link } from "react-router-dom";
 
 function SliderImg2() {
-    return (
-      <div className="relative">
-        <img
-          src="https://i.ibb.co/ysQnkNR/20190731-Pic-Blogbeitrag-Onboarding.webp"
-          alt="Normal User"
-          className="w-full h-[450px]  inset-0 bg-black opacity-50"
-        />
-  
-        <div className="absolute inset-0 flex flex-col items-center justify-center gap-y-4">
-          <h2 className="text-5xl text-gray-50">
-            Welcome to <span className="text-blue-700">TrackMyAssets</span>
-          </h2>
-          <p className="text-2xl text-gray-100">
-            {" "}
-            Get Started with Asset Management Join Us to Streamline Your Workflow
-          </p>
-          <Link to='/join-employee'
-            type="button"
-            className=" text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-lg px-7 py-3.5 me-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800"
-          >
-            Join as an Employee
-          </Link>
-        </div>
+  return (
+    <div className="relative h-[450px]">
+      {/* Background image */}
+      <div
+        className="absolute inset-0 bg-cover bg-center opacity-50"
+        style={{
+          backgroundImage: "url('https://i.ibb.co/hcTpk6Q/pexels-mikhail-nilov-9301259.jpg')",
+        }}
+      ></div>
+
+      {/* Content Section */}
+      <div className="absolute inset-0 flex flex-col items-center justify-center gap-y-4">
+        <h2 className="text-5xl text-gray-50">
+          Welcome to <span className="text-blue-700">TrackMyAssets</span>
+        </h2>
+        <p className="text-2xl text-gray-100">
+          Get Started with Asset Management Join Us to Streamline Your Workflow
+        </p>
+        <Link
+          to="/join-employee"
+          className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-lg px-7 py-3.5 me-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800"
+        >
+          Join as an Employee
+        </Link>
       </div>
-    );
-  }
-  
-  export default SliderImg2;
-  
+    </div>
+  );
+}
+
+export default SliderImg2;
